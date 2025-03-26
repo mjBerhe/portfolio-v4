ebd99922a8410b997f62842dae07bee0aa441e01
Matthew Berhe <matt@da:65:84:dd:68:1d.home>
2025-03-25 20:03:36 -0400

    adding framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import './App.css'
+import "./App.css";
+import { motion } from "motion/react";
 
 function App() {
   return (
-    <div className="bg-white text-black">hello</div>
-  )
+    <main className="h-screen bg-black w-screen text-white">
+      <div className="container border border-white mx-auto p-6">
+        <motion.div
+          animate={{ scale: 0.5 }}
+          className="border border-slate-500 p-4 my-4"
+        >
+          example box
+        </motion.div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
