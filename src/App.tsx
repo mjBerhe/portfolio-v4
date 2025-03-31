ccebd5116a15caf8e6404de03424eff71c6f3056
Matthew Berhe <matt@da:65:84:dd:68:1d.home>
2025-03-31 19:43:16 -0400

    background + some motions

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import "./App.css";
 import { motion } from "motion/react";
+import { ShootingStars } from "./components/ui/shooting-stars";
+import { StarsBackground } from "./components/ui/stars-background";
 
 function App() {
   return (
     <main className="h-screen bg-black w-screen text-white">
-      <div className="container border border-white mx-auto p-6">
-        <motion.div
-          animate={{ scale: 0.5 }}
-          className="border border-slate-500 p-4 my-4"
-        >
-          example box
-        </motion.div>
+      <div className="min-h-screen container mx-auto ">
+        <div className="flex flex-col gap-y-4 w-full pt-4">
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            className="border border-slate-500 p-4 w-full h-[200px]"
+          >
+            Card 1
+          </motion.div>
+          <motion.div className="border border-slate-500 p-4 my-4 w-full h-[200px]">
+            Card 2
+          </motion.div>
+        </div>
       </div>
+
+      <ShootingStars />
+      <StarsBackground />
     </main>
   );
 }
