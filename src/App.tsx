d031377a36b8206ed98c110855f82ac1e43aa00c
Matthew Berhe <matt@da:65:84:dd:68:1d.home>
2025-04-02 19:21:39 -0400

    better motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
 import "./App.css";
+import { cn } from "./lib/utils";
 import { motion } from "motion/react";
 import { ShootingStars } from "./components/ui/shooting-stars";
 import { StarsBackground } from "./components/ui/stars-background";
 
 function App() {
+  //    const test = [3, 2, 1]
   return (
     <main className="h-screen bg-gray-950 w-screen text-white">
-      <div className="min-h-screen container mx-auto ">
-        <div className="flex flex-col gap-y-4 w-full pt-4">
+      <div className="container mx-auto max-w-[1024px] px-8">
+        <div className="flex flex-col gap-y-4 pt-8 max-w-[1024px]">
           <motion.div
-            initial={{ opacity: 0, y: 50 }}
+            initial={{ opacity: 0.8, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
-            className="border border-gray-800/50 rounded-xl relative z-100 p-4 w-full h-[200px] bg-gray-900 shadow-xl"
+            transition={{ duration: 0.6, ease: "easeOut" }}
+            className={cn("border border-gray-700 bg-gradient-to-br from-gray-900 to-black ",
+              "rounded-xl relative z-100 p-4 w-full h-[200px] shadow-2xl")}
           >
-            Card 1
+            Hello, I'm Matt
           </motion.div>
-          <motion.div className="border border-slate-500 p-4 w-full h-[200px]">
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.5, duration: 0.6, ease: "easeOut" }}
+
+            className={cn("border border-gray-700 bg-gradient-to-br from-gray-900 to-black ",
+              "rounded-xl relative z-100 p-4 w-full h-[200px] shadow-2xl")}>
             Card 2
           </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 1.0, duration: 0.6, ease: "easeOut" }}
+
+            className={cn("border border-gray-700 bg-gradient-to-br from-gray-900 to-black ",
+              "rounded-xl relative z-100 p-4 w-full h-[200px] shadow-2xl")}>
+            Card 3
+          </motion.div>
+
         </div>
       </div>
 
